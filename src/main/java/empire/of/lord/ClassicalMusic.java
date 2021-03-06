f3e3ff3651ac45016b3d44c887e41016eb38a6db
ILMentor <dekstroer8202@gmail.com>
2021-03-06 18:45:42 +0100

    Init, destroy, fabric methods

--- src/main/java/empire/of/lord/ClassicalMusic.java
+++ src/main/java/empire/of/lord/ClassicalMusic.java
@@ -1,6 +1,20 @@
 package empire.of.lord;
 
 public class ClassicalMusic implements Music{
+
+    private ClassicalMusic(){}
+
+    public static ClassicalMusic getClassicalMusic(){
+        return new ClassicalMusic();
+    }
+
+    public void doMyInit(){
+        System.out.println("Do my initialization");
+    }
+
+    public void doMyDestroy(){
+        System.out.println("Do my destroy");
+    }
     @Override
     public String getSong() {
         return "Rhapsody";
