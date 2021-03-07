b8f3545a9b882888859d0ee4af6893a71d272c88
ILMentor <dekstroer8202@gmail.com>
2021-03-07 10:07:00 +0100

    Use annotation Qualifier

--- src/main/java/empire/of/lord/MusicPlayer.java
+++ src/main/java/empire/of/lord/MusicPlayer.java
@@ -1,41 +1,23 @@
 package empire.of.lord;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MusicPlayer {
-
-    private ClassicalMusic classicalMusic;
-    private RockMusic rockMusic;
-    private String name;
-    private int volume;
+    private Music music1;
+    private Music music2;
 
     @Autowired
-    public MusicPlayer(ClassicalMusic classicalMusic, RockMusic rockMusic) {
-        this.classicalMusic = classicalMusic;
-        this.rockMusic = rockMusic;
+    public MusicPlayer(@Qualifier("rockMusic") Music music1,
+                       @Qualifier("classicalMusic") Music music2) {
+        this.music1 = music1;
+        this.music2 = music2;
     }
 
     public String playMusic(){
-        return classicalMusic.getSong();
+        return music1.getSong() + ", " + music2.getSong();
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getVolume() {
-        return volume;
-    }
-
-    public void setVolume(int volume) {
-        this.volume = volume;
-    }
-
-    public MusicPlayer() {}
 }
