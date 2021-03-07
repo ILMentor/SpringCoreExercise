a25b509e69e00beb0cefe06549f1e62834ec8859
ILMentor <dekstroer8202@gmail.com>
2021-03-07 11:12:40 +0100

    Use Java class configuration to configure all dependencies in project instead of @ComponentScan and @Component

--- src/main/java/empire/of/lord/SpringConfig.java
+++ src/main/java/empire/of/lord/SpringConfig.java
@@ -1,12 +1,30 @@
 package empire.of.lord;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@ComponentScan("empire.of.lord")
 @PropertySource("classpath:musicPlayer.properties")
 public class SpringConfig {
+    @Bean
+    public ClassicalMusic classicalMusic(){
+        return new ClassicalMusic();
+    }
 
+    @Bean
+    public RockMusic rockMusic(){
+        return new RockMusic();
+    }
+
+    @Bean
+    public MusicPlayer musicPlayer(){
+        return new MusicPlayer(classicalMusic(), rockMusic());
+    }
+
+    @Bean
+    public Computer computer(){
+        return new Computer(musicPlayer());
+    }
 }
